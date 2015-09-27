c5f8b57d29867283173e1b49e8a89d72ac9f38e9
Varun Shastry <vshastry@redhat.com>
2015-09-27 01:17:46 -0400

    Additions
    Signed-off-by: Varun Shastry <vshastry@redhat.com>

--- 1/tcpechotimecli.c
+++ 1/tcpechotimecli.c
@@ -93,13 +93,15 @@ handle_request(struct in_addr *ip, int choice)
     default:
       close (fd[1]);
 
-      while (read(pipefd[0], buf, MAX_BUF_SIZE) != 0 /* EOF */)
-        logit(INFO, buf);
-
       wait(NULL);
+      /*
+      while (read(pipefd[0], buf, MAX_BUF_SIZE) != 0 * EOF *)
+        logit(INFO, buf);
+        */
+
       close (pipefd[0]);
   }
-  close(pipefd[0]);
+  close(pipefd[1]);
 }
 
 int
@@ -109,7 +111,7 @@ process(struct in_addr *ip)
 
   while (42) {
     printf ("\n1. echo server\n2. time server\n3. Exit\nEnter your choice: ");
-    if (scanf ("%d", &choice) != 1) {
+    if (scanf ("%d", &choice) == -100) {
       logit (ERROR, "User input error to scanf");
       continue;
     }
@@ -128,6 +130,7 @@ process(struct in_addr *ip)
         printf ("Invalid choice\n");
         break;
     }
+    fflush (stdin);
   }
 }
 
